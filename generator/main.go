58b0605c7d924407e676282b30374bad5438ae30
Carson Ip <carson.ip@elastic.co>
2025-07-23 15:29:27 +0100

    Update generator

--- generator/main.go
+++ generator/main.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"sync/atomic"
 	"time"
 
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/trace"
 )
@@ -38,12 +40,16 @@ func runTransactions(tracerProvider *trace.TracerProvider) {
 	var wg sync.WaitGroup
 	var doneCh = make(chan struct{})
 
+	slowTxnID := atomic.Int64{}
 	// Slow threads: each sends a slow transaction with configurable duration
 	for i := 0; i < *numSlow; i++ {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
-			_, span := tracer.Start(ctx, fmt.Sprintf("slow-transaction-%d", id))
+			_, span := tracer.Start(ctx, fmt.Sprintf("slow-transaction-%d", slowTxnID.Add(1)))
+			span.SetAttributes(
+				attribute.String("thread_id", fmt.Sprintf("slow-%d", id)),
+			)
 			time.Sleep(*slowDuration)
 			span.End()
 			fmt.Printf("Slow transaction %d finished\n", id)
@@ -51,6 +57,7 @@ func runTransactions(tracerProvider *trace.TracerProvider) {
 		}(i)
 	}
 
+	fastTxnID := atomic.Int64{}
 	// Fast threads: send fast transactions as quickly as possible
 	for i := 0; i < *numFast; i++ {
 		wg.Add(1)
@@ -62,7 +69,10 @@ func runTransactions(tracerProvider *trace.TracerProvider) {
 					return
 				default:
 				}
-				_, span := tracer.Start(ctx, fmt.Sprintf("fast-transaction-%d", id))
+				_, span := tracer.Start(ctx, fmt.Sprintf("fast-transaction-%d", fastTxnID.Add(1)))
+				span.SetAttributes(
+					attribute.String("thread_id", fmt.Sprintf("fast-%d", id)),
+				)
 				span.End()
 				// Optionally, sleep for a tiny amount to avoid overwhelming the exporter
 				time.Sleep(10 * time.Millisecond)
