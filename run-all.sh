c08c414ca18e75471c8c90516fcf95d19d26022c
Carson Ip <carson.ip@elastic.co>
2025-07-23 21:03:34 +0100

    Parametrize concurrency

--- run-all.sh
+++ run-all.sh
@@ -2,7 +2,9 @@
 
 for OFFLOAD in true false; do
   for PAYLOAD_SIZE in 10000 100000; do
-    echo "Running with offload_to_disk=$OFFLOAD, payload_size=$PAYLOAD_SIZE"
-    bash run-one.sh "$OFFLOAD" "$PAYLOAD_SIZE" 2>&1 | grep -E 'Sent|CountExporter|RSS' | sed 's/^/  /'
+    for CONCURRENCY in 1 10; do
+      echo "Running with offload_to_disk=$OFFLOAD, payload_size=$PAYLOAD_SIZE, concurrency=$CONCURRENCY"
+      bash run-one.sh "$OFFLOAD" "$PAYLOAD_SIZE" "$CONCURRENCY" 2>&1 | grep -E 'Sent|CountExporter|RSS' | sed 's/^/  /'
+    done
   done
 done
--- run-one.sh
+++ run-one.sh
@@ -2,6 +2,7 @@
 
 OFFLOAD_TO_DISK="$1"
 PAYLOAD_SIZE="$2"
+CONCURRENCY="$3"
 # Start the tail collector with offload_to_disk override
 ./_build/otel-tbs-playground --config tail.yaml --set processors.tail_sampling.offload_to_disk="$OFFLOAD_TO_DISK" &
 TAIL_PID=$!
@@ -22,10 +23,21 @@ exec 3< <(
     echo "$MAX_RSS"
 )
 
- # Run the Go generator
-(cd generator && OTEL_SERVICE_NAME=generator OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317 OTEL_EXPORTER_OTLP_INSECURE=true go run main.go -slow=1 -fast=100 -slow-duration=3s -payload-size="$PAYLOAD_SIZE")
+# Run the Go generator with concurrency
+
+GENERATOR_PIDS=()
+for ((i=0; i<CONCURRENCY; i++)); do
+  (cd generator && OTEL_SERVICE_NAME=generator OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317 OTEL_EXPORTER_OTLP_INSECURE=true go run main.go -slow=1 -fast=100 -slow-duration=3s -payload-size="$PAYLOAD_SIZE") &
+  GENERATOR_PIDS+=("$!")
+done
+
+# Wait for all generator processes to finish
+for pid in "${GENERATOR_PIDS[@]}"; do
+  wait "$pid"
+done
 
 sleep 10
+
 kill $TAIL_PID
 read -u 3 MAX_RSS
 echo "tail collector RSS high watermark: ${MAX_RSS} KB"
