d038fbcf007f270dd4b927bc842d36260cb0c0d1
Carson Ip <carson.ip@elastic.co>
2025-07-24 17:01:05 +0100

    Update grep and duration

--- run-all.sh
+++ run-all.sh
@@ -4,7 +4,7 @@ for OFFLOAD in true false; do
   for PAYLOAD_SIZE in 10000 100000; do
     for CONCURRENCY in 1 10; do
       echo "Running with offload_to_disk=$OFFLOAD, payload_size=$PAYLOAD_SIZE, concurrency=$CONCURRENCY"
-      bash run-one.sh "$OFFLOAD" "$PAYLOAD_SIZE" "$CONCURRENCY" 2>&1 | grep -E 'Sent|CountExporter|RSS' | sed 's/^/  /'
+      bash run-one.sh "$OFFLOAD" "$PAYLOAD_SIZE" "$CONCURRENCY" 2>&1 | grep -E 'Sent|CountExporter|RSS|db size' | sed 's/^/  /'
     done
   done
 done
